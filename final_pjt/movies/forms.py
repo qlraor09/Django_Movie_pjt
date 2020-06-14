4ccabd279a04225335ea18684c4e6f582581d1f1
qlraor09 <qlraor10@gmail.com>
2020-06-15 02:35:17 +0900

    genre_choice

--- final_pjt/movies/forms.py
+++ final_pjt/movies/forms.py
@@ -3,6 +3,11 @@ from .models import Review, Comment, Movie, Genre
 
 
 class MovieForm(forms.ModelForm):
+    genre_ids = forms.ModelMultipleChoiceField(
+        label = 'Genre',
+        queryset = Genre.objects.all(),
+        widget = forms.CheckboxSelectMultiple,
+    )
     class Meta:
         model = Movie
         fields = ["title", "original_title", "release_date", "popularity", "vote_count", "vote_average", "adult", "video", "overview", "original_language", "poster_path", "backdrop_path", "genre_ids",]
